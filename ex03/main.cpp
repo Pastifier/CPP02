7089098f7f4232cd3f9c5ae06f2b25756c22c2d3
Pastifier <136130975+Pastifier@users.noreply.github.com>
2024-08-20 12:58:26 +0000

    Added more tests in main() and made output-format readable

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,19 +6,63 @@ int main() {
     Point c(0, 5);
     Point point(1, 1);
 
-    Fixed d = 1;
-    Fixed e = 0;
-
-    try {
-        std::cout << d / e << std::endl;
-    } catch (const std::invalid_argument& e) {
-        std::cerr << e.what() << std::endl;
-    }
+    std::cout << "\n------ - - -- - ---- -- --- - -- --- -- \n" << std::endl;   
+    std::cout << "For vertices { (0,0) , (5,0) , (0,5) }: \n" << std::endl;
+    std::cout << "------ - - -- - ---- -- --- - -- --- -- \n" << std::endl;
     if (bsp(a, b, c, point)) {
-        std::cout << "Point is inside the triangle.\n";
+        std::cout << "Point (1,1) is inside the triangle.\n";
     } else {
-        std::cout << "Point is outside the triangle.\n";
+        std::cout << "Point (1,1) is outside the triangle.\n";
     }
 
+    if (bsp(a, b, c, Point(100, 100))) {
+        std::cout << "Point (100,100) is inside the triangle.\n";
+    } else {
+        std::cout << "Point (100,100) is outside the triangle.\n";
+    }
+
+    if (bsp(a, b, c, Point(0, 5))) {
+        std::cout << "Point (0,5) is inside the triangle.\n";
+    } else {
+        std::cout << "Point (0,5) is outside the triangle.\n";
+    }
+
+    if (bsp(a, b, c, Point(5, 0))) {
+        std::cout << "Point (5,0) is inside the triangle.\n";
+    } else {
+        std::cout << "Point (5,0) is outside the triangle.\n";
+    }
+    std::cout << "------ - - -- - ---- -- --- - -- --- -- \n" << std::endl;
+
+    a = Point(-2, -15);
+    b = Point(-15, -2);
+    c = Point(0, 0);
+    std::cout << "For vertices { (-2,-15) , (-15,-2) , (0,0) }: \n" << std::endl;
+    std::cout << "------ - - -- - ---- -- --- - -- --- -- \n" << std::endl;
+    if (bsp(a, b, c, point)) {
+        std::cout << "Point (1,1) is inside the triangle.\n";
+    } else {
+        std::cout << "Point (1,1) is outside the triangle.\n";
+    }
+
+    if (bsp(a, b, c, Point(0, -1))) {
+        std::cout << "Point (0,-1) is inside the triangle.\n";
+    } else {
+        std::cout << "Point (0,-1) is outside the triangle.\n";
+    }
+
+    if (bsp(a, b, c, Point(-500, 5))) {
+        std::cout << "Point (-500,5) is inside the triangle.\n";
+    } else {
+        std::cout << "Point (-500,5) is outside the triangle.\n";
+    }
+
+    if (bsp(a, b, c, Point(-12, -13))) {
+        std::cout << "Point (-12, -13) is inside the triangle.\n";
+    } else {
+        std::cout << "Point (-12, -13) is outside the triangle.\n";
+    }
+    std::cout << "------ - - -- - ---- -- --- - -- --- -- \n" << std::endl;
+    
     return 0;
 }
